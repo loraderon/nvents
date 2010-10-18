97428c1f5e361c9162de50bf91544723cf48eb3a
loraderon <anders@waglund.net>
2010-10-18 12:12:01 +0200

    removed unused IEventServiceChannel.cs

--- src/Nvents/Services/Network/IEventServiceChannel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Nvents.Services.Network
-{
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-	public interface IEventServiceChannel : IEventService, System.ServiceModel.IClientChannel
-	{
-	}
-}
