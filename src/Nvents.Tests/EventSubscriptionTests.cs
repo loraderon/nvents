4b7d0ac9bed2215b718a85f9aebf52ff87ae2a7b
loraderon <anders@waglund.net>
2010-12-14 13:32:36 +0100

    added EventSubscription to enable subscribing to events using standard .NET events

---
+++ src/Nvents.Tests/EventSubscriptionTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Nvents;
+using Xunit;
+
+namespace Nvents.Tests
+{
+	public class EventSubscriptionTests
+	{
+		[Fact]
+		public void ShouldRaiseEventPublishedOnPublish()
+		{
+			var raised = false;
+			var fooEvents = new EventSubscription<FooEvent>();
+			fooEvents.Published += (s, e) =>
+				raised = true;
+
+			Events.Publish(new FooEvent());
+			System.Threading.Thread.Sleep(100);
+
+			Assert.True(raised);
+		}
+
+		[Fact]
+		public void ShouldRaiseEventPublishedForInheritedEvents()
+		{
+			var raised = false;
+			var fooEvents = new EventSubscription<FooEvent>();
+			fooEvents.Published += (s, e) =>
+				raised = true;
+
+			Events.Publish(new FooChildEvent());
+			System.Threading.Thread.Sleep(100);
+
+			Assert.True(raised);
+		}
+
+		[Fact]
+		public void ShouldRaiseEventPublishedForInheritedEventSubscription()
+		{
+			var raised = false;
+			var fooEvents = new FooEventSubscription();
+			fooEvents.Published += (s, e) =>
+				raised = true;
+
+			Events.Publish(new FooEvent());
+			System.Threading.Thread.Sleep(100);
+
+			Assert.True(raised);
+		}
+
+		public EventSubscriptionTests()
+		{
+			Events.Service = new Services.InProcessService();
+		}
+
+		public class FooEventSubscription : EventSubscription<FooEvent>
+		{ }
+	}
+}
---
+++ src/Nvents/EventSubscription.cs
@@ -0,0 +1,37 @@
+using System;
+using Nvents.Services;
+
+namespace Nvents
+{
+	public class EventSubscription<TEvent> where TEvent : class, IEvent
+	{
+		public event EventHandler<PublishedEventArgs> Published;
+
+		public EventSubscription(Func<TEvent, bool> filter = null)
+			: this(Events.Service, filter)
+		{ }
+
+		public EventSubscription(ISubscriber subscriber, Func<TEvent, bool> filter = null)
+		{
+			subscriber.Subscribe<TEvent>(
+				e => OnPublished(e),
+				filter);
+		}
+
+		protected virtual void OnPublished(TEvent @event)
+		{
+			if (Published != null)
+				Published(this, new PublishedEventArgs(@event));
+		}
+
+		public class PublishedEventArgs : EventArgs
+		{
+			public readonly TEvent Event;
+
+			public PublishedEventArgs(TEvent @event)
+			{
+				Event = @event;
+			}
+		}
+	}
+}
