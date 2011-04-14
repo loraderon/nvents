9d2fef3b5e2a00d0c4eab723f11f62117eb1e560
loraderon <anders@waglund.net>
2011-04-14 09:55:48 +0200

    EncryptionTests using EncryptableNetDataContractSerializer directly

--- src/Nvents.Tests/EncryptionTests.cs
+++ src/Nvents.Tests/EncryptionTests.cs
@@ -1,39 +1,64 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+using Nvents.Services.Network;
 using Xunit;
-using Nvents.Services;
-using System.Threading;
 
 namespace Nvents.Tests
 {
 	public class EncryptionTests
 	{
 		[Fact]
-		public void UnencryptedSerivceShouldNotGetEncryptedEvents()
+		public void UnencryptedSerializerCannotDeserializeEncryptedEvent()
 		{
-			var encrypted = new AutoNetworkService(encryptionKey: "encryption-key");
-			encrypted.Start();
-			var unecrypted = new AutoNetworkService();
-			unecrypted.Start();
+			var e = new FooEvent { Baz = "Bar" };
+			string serializedEncrypted = Serialize(encrypted, e);
 
-			bool raisedEncrypted = false;
-			bool raisedUnencrypted = false;
-
-			encrypted
-				.Subscribe<FooEvent>(e => raisedEncrypted = true);
-			unecrypted
-				.Subscribe<FooEvent>(e => raisedUnencrypted = true);
-
-			var started = DateTime.Now;
-			encrypted.Publish(new FooEvent());
-
-			var timeout = TimeSpan.FromSeconds(2);
-			while ((DateTime.Now - started) < timeout)
-			{
-				Thread.Sleep(100);
-			}
-
-			Assert.True(raisedEncrypted, "Encrypted subscription was not raised.");
-			Assert.False(raisedUnencrypted, "Unencrypted subscription was raised.");
+			Assert.Throws<SerializationException>(() => 
+				Deserialize(plain, serializedEncrypted));
 		}
+
+		[Fact]
+		public void EncryptedSerializerCanDeserializeEncryptedEvent()
+		{
+			var e = new FooEvent { Baz = "Bar" };
+			string serializedEncrypted = Serialize(encrypted, e);
+
+			var deserialized = Deserialize(encrypted, serializedEncrypted) as FooEvent;
+			
+			Assert.NotNull(deserialized);
+			Assert.Equal("Bar", deserialized.Baz);
+		}
+
+		private string Serialize(EncryptableNetDataContractSerializer serializer, object graph)
+		{
+			using (var stream = new MemoryStream())
+			{
+				using (var writer = new XmlTextWriter(stream, Encoding.UTF8))
+				{
+					serializer.WriteStartObject(writer, graph);
+					serializer.WriteObjectContent(writer, graph);
+					serializer.WriteEndObject(writer);
+					writer.Flush();
+					return Encoding.UTF8.GetString(stream.ToArray());
+				}
+			}
+		}
+
+		private object Deserialize(EncryptableNetDataContractSerializer serializer, string data)
+		{
+			using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(data)))
+			{
+				using (var reader = new XmlTextReader(stream))
+				{
+					return serializer.ReadObject(reader);
+				}
+			}
+		}
+
+		EncryptableNetDataContractSerializer encrypted = new EncryptableNetDataContractSerializer("encryption-key");
+		EncryptableNetDataContractSerializer plain = new EncryptableNetDataContractSerializer(null);
 	}
 }
